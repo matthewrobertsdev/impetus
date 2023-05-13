14f90a0594244f699951623aa52fba61a3bab74a
Matt <31932179+matthewrobertsdev@users.noreply.github.com>
2023-05-13 00:12:53 -0400

    Start on frontend of app.

--- impetus.py
+++ impetus.py
@@ -1,8 +1,22 @@
 from tkinter import *
   
 root = Tk()
+root.title('impetus')
 root.minsize(400,400)
-a = Label(root, text ="Hello World")
-a.pack()
-  
+#a = Label(root, text ="Hello World")
+#a.pack()
+root.wm_attributes("-transparent", True)
+#root.config(bg='systemTransparent')
+#f1 = Frame(root, width=400, height=400)
+#f1.configure(background='black'), 
+#f1.pack(expand=True)
+output_display = Text(root);
+output_display.pack(side="top", fill=BOTH, expand=True)
+output_display.configure(state=DISABLED, insertbackground='#00ff00', highlightcolor='#00ff00')
+bottom_frame = Frame(root, width=400, height=40)
+bottom_frame.pack(fill=X, side="bottom", expand=False)
+command_entry = Text(bottom_frame, width=60, height=7);
+command_entry.pack(fill=X, expand=False)
+command_entry.configure(insertbackground='#00ff00', blockcursor=True, highlightcolor='#00ff00', fg='#00ff00')
+command_entry.focus_set()
 root.mainloop()
